e64c507f9f656aae6bafa5cdee4d6637f0f94652
Michele Picozzi <michele.picozzi@hotmail.it>
2025-06-06 09:50:53 +0100

    adds my own svg map

---
+++ app/(root)/world-map/page.tsx
@@ -0,0 +1,10 @@
+import WorldMapSvg from "@/public/svg/WorldMapSvg";
+import React from "react";
+
+export default function Page() {
+    return (
+        <div>
+            <WorldMapSvg />
+        </div>
+    );
+}
