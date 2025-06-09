c4ab9a60143b1e8a3e6e6aaa8199801296897c5e
Michele Picozzi <michele.picozzi@hotmail.it>
2025-06-09 12:15:48 +0100

    better svg map

--- app/(root)/world-map/page.tsx
+++ app/(root)/world-map/page.tsx
@@ -1,10 +1,31 @@
+"use client";
+import React, { useState } from "react";
+
 import WorldMapSvg from "@/public/svg/WorldMapSvg";
 
 export default function Page() {
+    const [hoveredCountry, setHoveredCountry] = useState<string | null>(null);
+
+    const handleMouseOver = (e: React.MouseEvent<SVGElement>) => {
+        const target = e.target as SVGElement;
+        const countryId = target.id;
+
+        if (countryId) {
+            setHoveredCountry(countryId.toUpperCase());
+        }
+    };
+
+    const handleMouseOut = () => {
+        setHoveredCountry(null);
+    };
+
     return (
-        <div className="hover:text-green-600">
+        <div className="">
             <h1>SVG Map</h1>
-            <WorldMapSvg />
+            <div className="mb-2 text-center text-lg font-semibold h-6">
+                {hoveredCountry ?? "Hover over a country"}
+            </div>
+            <WorldMapSvg onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
         </div>
     );
 }
