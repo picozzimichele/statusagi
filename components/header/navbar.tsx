08328b3259382121b0d7ea863476a418e68c2476
Michele Picozzi <michele.picozzi@hotmail.it>
2025-06-11 16:03:17 +0100

    makes a small navbar

--- components/header/navbar.tsx
+++ components/header/navbar.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/ui/mode-toggle";
-import { Separator } from "@radix-ui/react-dropdown-menu";
 import { SidebarIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import { useSidebar } from "@/components/ui/sidebar";
@@ -11,14 +10,15 @@ import { useSidebar } from "@/components/ui/sidebar";
 export default function Navbar() {
     const { toggleSidebar } = useSidebar();
     return (
-        <header className="sticky top-0 z-50 flex w-full items-center border-b">
-            <div className="flex h-(--header-height) w-full items-center gap-2 px-4">
+        <header className="sticky top-0 z-50 flex w-full items-center border-b bg-background">
+            <div className="flex h-12 w-full items-center gap-2 px-4">
                 <Button className="h-8 w-8" variant="ghost" size="icon" onClick={toggleSidebar}>
                     <SidebarIcon />
                 </Button>
-                <div className="mr-2 rotate-90 flex flex-1 border-[0.5px] border-green-200" />
+                <div className="flex h-12 p-2 border-l" />
                 <div className="flex justify-between h-10 w-[95%] items-center mx-auto">
                     <Logo />
+
                     <ModeToggle />
                 </div>
             </div>
